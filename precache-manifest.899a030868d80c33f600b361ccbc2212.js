384d46827eea2f4143bf6ac66218fc19fc6a4e2b
Juliâncio <juliancio_alves@hotmail.com>
2020-02-27 15:47:40 -0300

    Updates

---
+++ precache-manifest.899a030868d80c33f600b361ccbc2212.js
@@ -0,0 +1,642 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54f3c320fa347f31be2f3461ca9c059a",
+    "url": "/swapi-react-app/index.html"
+  },
+  {
+    "revision": "15f5cca9fbf6474db9e5",
+    "url": "/swapi-react-app/static/css/2.a7daffc7.chunk.css"
+  },
+  {
+    "revision": "64c36f410df785d8f185",
+    "url": "/swapi-react-app/static/css/main.6383107e.chunk.css"
+  },
+  {
+    "revision": "15f5cca9fbf6474db9e5",
+    "url": "/swapi-react-app/static/js/2.985651c7.chunk.js"
+  },
+  {
+    "revision": "d7b319d8a6f4d5773bf6cc977c7731d1",
+    "url": "/swapi-react-app/static/js/2.985651c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64c36f410df785d8f185",
+    "url": "/swapi-react-app/static/js/main.d7308df2.chunk.js"
+  },
+  {
+    "revision": "a1b66ab2cdeb54667e6f",
+    "url": "/swapi-react-app/static/js/runtime-main.a600248f.js"
+  },
+  {
+    "revision": "0c91cd26daabc02c65c762172bcc639f",
+    "url": "/swapi-react-app/static/media/1.0c91cd26.jpg"
+  },
+  {
+    "revision": "884f91924ab45cb929b4c0393a64f8f3",
+    "url": "/swapi-react-app/static/media/1.884f9192.jpg"
+  },
+  {
+    "revision": "c5857f7d16a6c064bf27143efa18c87b",
+    "url": "/swapi-react-app/static/media/1.c5857f7d.jpg"
+  },
+  {
+    "revision": "027399d400670aa02dbe1ce4c9e6d2d1",
+    "url": "/swapi-react-app/static/media/10.027399d4.jpg"
+  },
+  {
+    "revision": "569a79d7a2160d4286d39700234deee4",
+    "url": "/swapi-react-app/static/media/10.569a79d7.jpg"
+  },
+  {
+    "revision": "74aeb9f85778237508ee914b26018e88",
+    "url": "/swapi-react-app/static/media/10.74aeb9f8.jpg"
+  },
+  {
+    "revision": "0bc16a0a02dd693522e32fa8ca1ae9c2",
+    "url": "/swapi-react-app/static/media/11.0bc16a0a.jpg"
+  },
+  {
+    "revision": "68e641fc2d34af05eae47a724b0bee0e",
+    "url": "/swapi-react-app/static/media/11.68e641fc.jpg"
+  },
+  {
+    "revision": "9b49cf599ab73d40545a39c30381a2ea",
+    "url": "/swapi-react-app/static/media/11.9b49cf59.jpg"
+  },
+  {
+    "revision": "286110cd395ce3860aa253ffbb0031f3",
+    "url": "/swapi-react-app/static/media/12.286110cd.jpg"
+  },
+  {
+    "revision": "5d11992fe94c813eaf4b4ee3d2307d62",
+    "url": "/swapi-react-app/static/media/12.5d11992f.jpg"
+  },
+  {
+    "revision": "fe65f9176c003e755bdc7c084b2e504e",
+    "url": "/swapi-react-app/static/media/12.fe65f917.jpg"
+  },
+  {
+    "revision": "0a14e9711d190c0acee4f366edb931a7",
+    "url": "/swapi-react-app/static/media/13.0a14e971.jpg"
+  },
+  {
+    "revision": "6c2c095ecadef794f7acc26ac1649be2",
+    "url": "/swapi-react-app/static/media/13.6c2c095e.jpg"
+  },
+  {
+    "revision": "86182056623c6d0573bae6610b891fe9",
+    "url": "/swapi-react-app/static/media/13.86182056.jpg"
+  },
+  {
+    "revision": "6c4a05bbddcc2056a57c89973cd92d68",
+    "url": "/swapi-react-app/static/media/14.6c4a05bb.jpg"
+  },
+  {
+    "revision": "edd1b2988876ce695539927a42c4b2ac",
+    "url": "/swapi-react-app/static/media/14.edd1b298.jpg"
+  },
+  {
+    "revision": "17a306dd9f8c7cad41daeb07e6ff1675",
+    "url": "/swapi-react-app/static/media/15.17a306dd.jpg"
+  },
+  {
+    "revision": "5bd21b68a0894117169f1113cca9bd98",
+    "url": "/swapi-react-app/static/media/15.5bd21b68.jpg"
+  },
+  {
+    "revision": "d96b00aa253e35c92ddc0b1282b96b9a",
+    "url": "/swapi-react-app/static/media/15.d96b00aa.jpg"
+  },
+  {
+    "revision": "6dbf61074679a9b392e03814f15aa5f7",
+    "url": "/swapi-react-app/static/media/16.6dbf6107.jpg"
+  },
+  {
+    "revision": "eff0d4282d2b146e8371fbc67977056e",
+    "url": "/swapi-react-app/static/media/16.eff0d428.jpg"
+  },
+  {
+    "revision": "15dfc26302c3fbe5e7bffb6c3369ae71",
+    "url": "/swapi-react-app/static/media/17.15dfc263.jpg"
+  },
+  {
+    "revision": "3fc661981c53a46364c30e21b16087c7",
+    "url": "/swapi-react-app/static/media/17.3fc66198.jpg"
+  },
+  {
+    "revision": "4ec2b99a13d62ba3ba97ad72c708237c",
+    "url": "/swapi-react-app/static/media/18.4ec2b99a.jpg"
+  },
+  {
+    "revision": "9d5c3c6ea89e23f8e486a27364306915",
+    "url": "/swapi-react-app/static/media/18.9d5c3c6e.jpg"
+  },
+  {
+    "revision": "33a0c6de4316bad0931baea6edfe1ec1",
+    "url": "/swapi-react-app/static/media/19.33a0c6de.jpg"
+  },
+  {
+    "revision": "3761e3cb1087c7ee8e150838b6a24e15",
+    "url": "/swapi-react-app/static/media/19.3761e3cb.jpg"
+  },
+  {
+    "revision": "6ff58b5dcbcf52d0703ec95eddc178e8",
+    "url": "/swapi-react-app/static/media/2.6ff58b5d.jpg"
+  },
+  {
+    "revision": "bd92629e8fbe3b63c9ccd73fa46ac1e1",
+    "url": "/swapi-react-app/static/media/2.bd92629e.jpg"
+  },
+  {
+    "revision": "ce3e60472412c87c1830f521a5ea25b9",
+    "url": "/swapi-react-app/static/media/2.ce3e6047.jpg"
+  },
+  {
+    "revision": "f95edbedd193ec9f609c234050282f90",
+    "url": "/swapi-react-app/static/media/2.f95edbed.jpg"
+  },
+  {
+    "revision": "ac3f9ecd7c3b07facc44aba5c7cacb5e",
+    "url": "/swapi-react-app/static/media/20.ac3f9ecd.jpg"
+  },
+  {
+    "revision": "2d324f24c3c2aabf0912e51c0e356cf2",
+    "url": "/swapi-react-app/static/media/21.2d324f24.jpg"
+  },
+  {
+    "revision": "e40924a3a5c2e06ee1ce157104700f3d",
+    "url": "/swapi-react-app/static/media/21.e40924a3.jpg"
+  },
+  {
+    "revision": "e9768c901ee463f7a9f3c8acf9d88040",
+    "url": "/swapi-react-app/static/media/21.e9768c90.jpg"
+  },
+  {
+    "revision": "7985854997e62d3c86ec04eb66b23650",
+    "url": "/swapi-react-app/static/media/22.79858549.jpg"
+  },
+  {
+    "revision": "bea5ebb31f70dc66b2a3e89dc9dd90b4",
+    "url": "/swapi-react-app/static/media/22.bea5ebb3.jpg"
+  },
+  {
+    "revision": "93ac2bc8d76a41ebbfe4b61711695cd4",
+    "url": "/swapi-react-app/static/media/23.93ac2bc8.jpg"
+  },
+  {
+    "revision": "d7ffd831041f6420cae8b2b522ceb915",
+    "url": "/swapi-react-app/static/media/23.d7ffd831.jpg"
+  },
+  {
+    "revision": "61d16f9ecb870b3a9dd374ee56beb1d2",
+    "url": "/swapi-react-app/static/media/24.61d16f9e.jpg"
+  },
+  {
+    "revision": "1a6e02aa5af7047f49d8b9909dca3ebf",
+    "url": "/swapi-react-app/static/media/25.1a6e02aa.jpg"
+  },
+  {
+    "revision": "a26582f2b1e0a0fb8be24fd4ee71c916",
+    "url": "/swapi-react-app/static/media/26.a26582f2.jpg"
+  },
+  {
+    "revision": "5ec5ac653668c8368bbb17a84721990d",
+    "url": "/swapi-react-app/static/media/27.5ec5ac65.jpg"
+  },
+  {
+    "revision": "8b1356390c69c0acf456d0315fc74e0d",
+    "url": "/swapi-react-app/static/media/27.8b135639.jpg"
+  },
+  {
+    "revision": "f05cd15591e9f45522cae6fa8b075b14",
+    "url": "/swapi-react-app/static/media/27.f05cd155.jpg"
+  },
+  {
+    "revision": "52abcae61c45b4fc0a2ed099fde86c22",
+    "url": "/swapi-react-app/static/media/28.52abcae6.jpg"
+  },
+  {
+    "revision": "ea5922ad802cdec8ac3bb9e1f728edf5",
+    "url": "/swapi-react-app/static/media/28.ea5922ad.jpg"
+  },
+  {
+    "revision": "29498378d2e16fba09c715a68a742f97",
+    "url": "/swapi-react-app/static/media/29.29498378.jpg"
+  },
+  {
+    "revision": "e9debd5c46a63f16faf96ca8184245fa",
+    "url": "/swapi-react-app/static/media/29.e9debd5c.jpg"
+  },
+  {
+    "revision": "638a556b4576fe32aac0abb0ce327856",
+    "url": "/swapi-react-app/static/media/3.638a556b.jpg"
+  },
+  {
+    "revision": "79c869eef60997646c59a0043bedaa22",
+    "url": "/swapi-react-app/static/media/3.79c869ee.jpg"
+  },
+  {
+    "revision": "93f0e0aa16f90b2dd521b2ae69de5d56",
+    "url": "/swapi-react-app/static/media/3.93f0e0aa.jpg"
+  },
+  {
+    "revision": "f4b4428e9279845dbf96d49d73210de5",
+    "url": "/swapi-react-app/static/media/3.f4b4428e.jpg"
+  },
+  {
+    "revision": "3315f4946907887381ceca72ec5121a8",
+    "url": "/swapi-react-app/static/media/30.3315f494.jpg"
+  },
+  {
+    "revision": "e4959648f7cfdff67340976692cf1492",
+    "url": "/swapi-react-app/static/media/31.e4959648.jpg"
+  },
+  {
+    "revision": "fda97c369fbe296b044a0aee9ca6a244",
+    "url": "/swapi-react-app/static/media/31.fda97c36.jpg"
+  },
+  {
+    "revision": "20c2f6606f5fcf49929903300eea7f67",
+    "url": "/swapi-react-app/static/media/32.20c2f660.jpg"
+  },
+  {
+    "revision": "19a6a47d3c100aa237bfc7d4229ac8bd",
+    "url": "/swapi-react-app/static/media/33.19a6a47d.jpg"
+  },
+  {
+    "revision": "5b767020433096ad95f2437053e8ccf8",
+    "url": "/swapi-react-app/static/media/34.5b767020.jpg"
+  },
+  {
+    "revision": "4d91ea50b0eeb42a8a8172c7bbc160ab",
+    "url": "/swapi-react-app/static/media/35.4d91ea50.jpg"
+  },
+  {
+    "revision": "912475710b186d35489e71d17db97b77",
+    "url": "/swapi-react-app/static/media/36.91247571.jpg"
+  },
+  {
+    "revision": "20d4a878f2dfe33c964533d3234ee768",
+    "url": "/swapi-react-app/static/media/37.20d4a878.jpg"
+  },
+  {
+    "revision": "42c4823512f639d9385e841a01c48703",
+    "url": "/swapi-react-app/static/media/38.42c48235.jpg"
+  },
+  {
+    "revision": "6c95588ff1d636f32a11d133b5469c50",
+    "url": "/swapi-react-app/static/media/39.6c95588f.jpg"
+  },
+  {
+    "revision": "6ec97d3bc1f38d9fd24675d764a955a1",
+    "url": "/swapi-react-app/static/media/39.6ec97d3b.jpg"
+  },
+  {
+    "revision": "03b57d3ff3e869d90742ebb272cab731",
+    "url": "/swapi-react-app/static/media/4.03b57d3f.jpg"
+  },
+  {
+    "revision": "053c239be325acc4e1302145e379895c",
+    "url": "/swapi-react-app/static/media/4.053c239b.jpg"
+  },
+  {
+    "revision": "0f26e9152f692ca68d3559a4c8fb5407",
+    "url": "/swapi-react-app/static/media/4.0f26e915.jpg"
+  },
+  {
+    "revision": "5455003e652292223ee24469e7639adc",
+    "url": "/swapi-react-app/static/media/4.5455003e.jpg"
+  },
+  {
+    "revision": "078c9e3cb1274d71e8ae2c72efaeefc8",
+    "url": "/swapi-react-app/static/media/40.078c9e3c.jpg"
+  },
+  {
+    "revision": "c493bd03ec300c83fcdba683a9e9c49a",
+    "url": "/swapi-react-app/static/media/40.c493bd03.jpg"
+  },
+  {
+    "revision": "46068ed7c6bf04dbaba832c448dd7a04",
+    "url": "/swapi-react-app/static/media/41.46068ed7.jpg"
+  },
+  {
+    "revision": "88f3ea2d80f5f34d639a076709b1e8d4",
+    "url": "/swapi-react-app/static/media/41.88f3ea2d.jpg"
+  },
+  {
+    "revision": "2f8633ba72750400a775de9249e717eb",
+    "url": "/swapi-react-app/static/media/42.2f8633ba.jpg"
+  },
+  {
+    "revision": "5c13b1ca3b43d9de13c6945a074ccc99",
+    "url": "/swapi-react-app/static/media/43.5c13b1ca.jpg"
+  },
+  {
+    "revision": "71af17b601a48692198e671476266b6e",
+    "url": "/swapi-react-app/static/media/43.71af17b6.jpg"
+  },
+  {
+    "revision": "f5f746efc6fec922f3dffc5d2836c463",
+    "url": "/swapi-react-app/static/media/44.f5f746ef.jpg"
+  },
+  {
+    "revision": "116d975cd6c3ecd5c7652b974a003c05",
+    "url": "/swapi-react-app/static/media/45.116d975c.jpg"
+  },
+  {
+    "revision": "56b0e2c12bf7fb6a6fdab620e7743155",
+    "url": "/swapi-react-app/static/media/46.56b0e2c1.jpg"
+  },
+  {
+    "revision": "0f606ed44d666c6ea6b947287f9c2379",
+    "url": "/swapi-react-app/static/media/47.0f606ed4.jpg"
+  },
+  {
+    "revision": "5ccb5f2865bad801b56cd3e5ce16168e",
+    "url": "/swapi-react-app/static/media/47.5ccb5f28.jpg"
+  },
+  {
+    "revision": "acde5508fac4597567757f4bfde0d45a",
+    "url": "/swapi-react-app/static/media/48.acde5508.jpg"
+  },
+  {
+    "revision": "b76b1fb808c0790768dacdbe4d7da447",
+    "url": "/swapi-react-app/static/media/48.b76b1fb8.jpg"
+  },
+  {
+    "revision": "0dd1c294c233bee2b8456e5a5dffc1de",
+    "url": "/swapi-react-app/static/media/49.0dd1c294.jpg"
+  },
+  {
+    "revision": "8dab78b4d0101554ab163aef2519658f",
+    "url": "/swapi-react-app/static/media/49.8dab78b4.jpg"
+  },
+  {
+    "revision": "02af6ce8c8544ae6cd6e807b4e6dd4de",
+    "url": "/swapi-react-app/static/media/5.02af6ce8.jpg"
+  },
+  {
+    "revision": "53c0b037b0f025b49c1991033a2f6ff9",
+    "url": "/swapi-react-app/static/media/5.53c0b037.jpg"
+  },
+  {
+    "revision": "9c94bdbc4f06654c5a4b44f9cb12788d",
+    "url": "/swapi-react-app/static/media/5.9c94bdbc.jpg"
+  },
+  {
+    "revision": "c3ee4e438db1be173433a814866922d5",
+    "url": "/swapi-react-app/static/media/5.c3ee4e43.jpg"
+  },
+  {
+    "revision": "b52a712843e59f277591363ab83de5cc",
+    "url": "/swapi-react-app/static/media/50.b52a7128.jpg"
+  },
+  {
+    "revision": "304cf85a8711267467103b2145bf90f6",
+    "url": "/swapi-react-app/static/media/51.304cf85a.jpg"
+  },
+  {
+    "revision": "1200ec7c1f5dc24d6b7e8f8da979d3ac",
+    "url": "/swapi-react-app/static/media/52.1200ec7c.jpg"
+  },
+  {
+    "revision": "fbb2197b93e4e2bbb6ef47943a26e0d9",
+    "url": "/swapi-react-app/static/media/52.fbb2197b.jpg"
+  },
+  {
+    "revision": "8c478726f580f680ab75f9809ece1ce4",
+    "url": "/swapi-react-app/static/media/53.8c478726.jpg"
+  },
+  {
+    "revision": "ed4e6f762b9e343c25388f8e09c74a6b",
+    "url": "/swapi-react-app/static/media/54.ed4e6f76.jpg"
+  },
+  {
+    "revision": "2f8fc579eb2dc5811c896c890175f3c4",
+    "url": "/swapi-react-app/static/media/55.2f8fc579.jpg"
+  },
+  {
+    "revision": "d28209d498be528351ddcb7c2817499c",
+    "url": "/swapi-react-app/static/media/56.d28209d4.jpg"
+  },
+  {
+    "revision": "8f19f58eb87b64fcbe92c27676f437db",
+    "url": "/swapi-react-app/static/media/57.8f19f58e.jpg"
+  },
+  {
+    "revision": "41a38b5ad304341607439f55f61e84c0",
+    "url": "/swapi-react-app/static/media/58.41a38b5a.jpg"
+  },
+  {
+    "revision": "6bce47ad1ef98ed4a1007fb9838a215a",
+    "url": "/swapi-react-app/static/media/58.6bce47ad.jpg"
+  },
+  {
+    "revision": "b069075460c4c759e1837bf62411a065",
+    "url": "/swapi-react-app/static/media/59.b0690754.jpg"
+  },
+  {
+    "revision": "d2e213f0e1fbe329dc4f293993e36142",
+    "url": "/swapi-react-app/static/media/59.d2e213f0.jpg"
+  },
+  {
+    "revision": "541e2406e07f1aed0b8c933afd1a58d5",
+    "url": "/swapi-react-app/static/media/6.541e2406.jpg"
+  },
+  {
+    "revision": "58a69a4b97bd8d230689979108b5a4c8",
+    "url": "/swapi-react-app/static/media/6.58a69a4b.jpg"
+  },
+  {
+    "revision": "dac799747cc03339b1a85b0887b7d05c",
+    "url": "/swapi-react-app/static/media/6.dac79974.jpg"
+  },
+  {
+    "revision": "57b527fd70eaef2ea7c5814b0d4a5390",
+    "url": "/swapi-react-app/static/media/60.57b527fd.jpg"
+  },
+  {
+    "revision": "62989ff04d7a75b54cf65ef2ddf3c47e",
+    "url": "/swapi-react-app/static/media/61.62989ff0.jpg"
+  },
+  {
+    "revision": "cc700283f5e3b31d4d7ac79aa07ce9aa",
+    "url": "/swapi-react-app/static/media/61.cc700283.jpg"
+  },
+  {
+    "revision": "d4f006769bcce0c7101b421790e24c54",
+    "url": "/swapi-react-app/static/media/61.d4f00676.jpg"
+  },
+  {
+    "revision": "141b76bef7fb8c3c06d96e73a506c0fe",
+    "url": "/swapi-react-app/static/media/62.141b76be.jpg"
+  },
+  {
+    "revision": "bac13734c76eabcd33aea13a483ce361",
+    "url": "/swapi-react-app/static/media/63.bac13734.jpg"
+  },
+  {
+    "revision": "df6917cc09c7e3f2723f54c3bedb5da4",
+    "url": "/swapi-react-app/static/media/63.df6917cc.jpg"
+  },
+  {
+    "revision": "05e746a88bd7adfc08972d1278412b88",
+    "url": "/swapi-react-app/static/media/64.05e746a8.jpg"
+  },
+  {
+    "revision": "3b9ecf63d2b87babc6e13c0a36c6f468",
+    "url": "/swapi-react-app/static/media/64.3b9ecf63.jpg"
+  },
+  {
+    "revision": "134b976fab5bebf0d7094e6cf985a07e",
+    "url": "/swapi-react-app/static/media/65.134b976f.jpg"
+  },
+  {
+    "revision": "39842f9a02d7d8948927f3b732f4552b",
+    "url": "/swapi-react-app/static/media/66.39842f9a.jpg"
+  },
+  {
+    "revision": "f032dbf2de1c9961efd1189c80d7d3ef",
+    "url": "/swapi-react-app/static/media/66.f032dbf2.jpg"
+  },
+  {
+    "revision": "3305c13a6c3fe00aa91604414d86e09d",
+    "url": "/swapi-react-app/static/media/67.3305c13a.jpg"
+  },
+  {
+    "revision": "cb621439d5047b2ce84bca9d71cb1756",
+    "url": "/swapi-react-app/static/media/68.cb621439.jpg"
+  },
+  {
+    "revision": "d5076cc581a1d55f123a091b42ce23bb",
+    "url": "/swapi-react-app/static/media/68.d5076cc5.jpg"
+  },
+  {
+    "revision": "7b7bf1fa443cce98e86a73295747fd78",
+    "url": "/swapi-react-app/static/media/69.7b7bf1fa.jpg"
+  },
+  {
+    "revision": "8f4b4f89f7b9389906039ca8f0752854",
+    "url": "/swapi-react-app/static/media/7.8f4b4f89.jpg"
+  },
+  {
+    "revision": "9b3e5ff459bdde43e6a806ed3426b7da",
+    "url": "/swapi-react-app/static/media/7.9b3e5ff4.jpg"
+  },
+  {
+    "revision": "db24a011903045c0d6669c10fd523214",
+    "url": "/swapi-react-app/static/media/7.db24a011.jpg"
+  },
+  {
+    "revision": "a629f191f6d05f3edbc20417a82e3200",
+    "url": "/swapi-react-app/static/media/70.a629f191.jpg"
+  },
+  {
+    "revision": "3d354b95dc9c6da2664a7838e3b1e48a",
+    "url": "/swapi-react-app/static/media/71.3d354b95.jpg"
+  },
+  {
+    "revision": "90166dc1b3d54e67b436724791a0b4d5",
+    "url": "/swapi-react-app/static/media/72.90166dc1.jpg"
+  },
+  {
+    "revision": "1c87faa0f4ef92fceb9d618fd8474b6c",
+    "url": "/swapi-react-app/static/media/73.1c87faa0.jpg"
+  },
+  {
+    "revision": "35bc679bb43660b628cbac11171aa1f9",
+    "url": "/swapi-react-app/static/media/74.35bc679b.jpg"
+  },
+  {
+    "revision": "a34f7ccbc76e3b445b0ea050e6d182cc",
+    "url": "/swapi-react-app/static/media/74.a34f7ccb.jpg"
+  },
+  {
+    "revision": "40294f3b0546de21fdb24df201f34e57",
+    "url": "/swapi-react-app/static/media/75.40294f3b.jpg"
+  },
+  {
+    "revision": "c612d4bc38d037f2b59641cda30744a5",
+    "url": "/swapi-react-app/static/media/75.c612d4bc.jpg"
+  },
+  {
+    "revision": "e0cd42d91cddfae12f072726b301b1df",
+    "url": "/swapi-react-app/static/media/76.e0cd42d9.jpg"
+  },
+  {
+    "revision": "903a70638e3626e1934d65ddb6a846a6",
+    "url": "/swapi-react-app/static/media/77.903a7063.jpg"
+  },
+  {
+    "revision": "b63e685db5497bd6e46b80c2992eba69",
+    "url": "/swapi-react-app/static/media/77.b63e685d.jpg"
+  },
+  {
+    "revision": "b7f7006cf0a8a3dd19f0701271ac85f4",
+    "url": "/swapi-react-app/static/media/78.b7f7006c.jpg"
+  },
+  {
+    "revision": "60f9ab69934df86c295c52453f1d4eba",
+    "url": "/swapi-react-app/static/media/79.60f9ab69.jpg"
+  },
+  {
+    "revision": "210c16d20a0f65f2050093c10f209b7e",
+    "url": "/swapi-react-app/static/media/8.210c16d2.jpg"
+  },
+  {
+    "revision": "c7033ad005e509e301b3a67f2862036b",
+    "url": "/swapi-react-app/static/media/8.c7033ad0.jpg"
+  },
+  {
+    "revision": "ccf3d7cdf4cb03243d2d3ddabf936d6f",
+    "url": "/swapi-react-app/static/media/80.ccf3d7cd.jpg"
+  },
+  {
+    "revision": "7659eb5c0f4c4bb67add2df3db0ce7cb",
+    "url": "/swapi-react-app/static/media/81.7659eb5c.jpg"
+  },
+  {
+    "revision": "3e27dcbde402bc4e272db14cdf322c4a",
+    "url": "/swapi-react-app/static/media/82.3e27dcbd.jpg"
+  },
+  {
+    "revision": "8dc8131e07b551f6f1ca069d4e0e89b0",
+    "url": "/swapi-react-app/static/media/83.8dc8131e.jpg"
+  },
+  {
+    "revision": "342fc9640d37cc456fd4c1bb406869f2",
+    "url": "/swapi-react-app/static/media/84.342fc964.jpg"
+  },
+  {
+    "revision": "71bcc52f9f5349f130d7b24029d8cdf7",
+    "url": "/swapi-react-app/static/media/85.71bcc52f.jpg"
+  },
+  {
+    "revision": "92350baec0c45dc1319517c29a84ab44",
+    "url": "/swapi-react-app/static/media/86.92350bae.jpg"
+  },
+  {
+    "revision": "86b756d75b6ca664faaf11b6cb399259",
+    "url": "/swapi-react-app/static/media/87.86b756d7.jpg"
+  },
+  {
+    "revision": "47cbba0b747cab53682cc59b3335ce4f",
+    "url": "/swapi-react-app/static/media/88.47cbba0b.jpg"
+  },
+  {
+    "revision": "14ad196d5e4f81293c6b8677f03260f1",
+    "url": "/swapi-react-app/static/media/9.14ad196d.jpg"
+  },
+  {
+    "revision": "389424ec70be9dc4325f45df79dfc1fb",
+    "url": "/swapi-react-app/static/media/9.389424ec.jpg"
+  },
+  {
+    "revision": "daac3de8db19ac57fada2bcd31f13903",
+    "url": "/swapi-react-app/static/media/9.daac3de8.jpg"
+  },
+  {
+    "revision": "e45c1aa9df3a68b8ffd6e692bc88f4a1",
+    "url": "/swapi-react-app/static/media/banner4.e45c1aa9.jpg"
+  }
+]);
